72e0daf9aa2dab6a98b499446588a5270e4a942e
Diego Gonzalez <gonzalezdiego93@outlook.com>
2017-01-27 23:32:29 -0500

    WOT

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var thetableview: UITableView!
+    
+    var emoji = ["😭","🗿","🤦🏻‍♂️","🙃","👀","😗","😩","💩"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        thetableview.dataSource = self
+        thetableview.delegate = self
+        
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return emoji.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emoji[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
